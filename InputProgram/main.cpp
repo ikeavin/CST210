8c588ccaa330535e93b06fbc6ad3a1ca1b7273cd
Kevin Ahlstrom <ikeavin@gmail.com>
2020-01-09 10:12:25 -0700

    Initial Commit

---
+++ InputProgram/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  InputProgram
+//
+//  Created by Kevin Ahlstrom on 1/9/20.
+//  Copyright © 2020 Kevin Ahlstrom. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
